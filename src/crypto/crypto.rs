6d2baad872814b42f92680f84e0658a1fcfddde8
Tobias <tobias.ambuehl@gmail.com>
2022-04-19 21:39:36 +0200

    Address derivation fixed

--- src/crypto/crypto.rs
+++ src/crypto/crypto.rs
@@ -1,6 +1,6 @@
 use ed25519_dalek_bip32::DerivationPath;
 use ed25519_dalek_bip32::ExtendedSecretKey;
-use sha3::{Digest, Sha3_256, Sha3_512};
+use sha3::{Digest, Sha3_256};
 
 pub fn derive_key(path: String, seed: &Vec<u8>) -> (Vec<u8>, Vec<u8>, Vec<u8>) {
     let dp: DerivationPath = parse_derivation_path(&path);
@@ -11,7 +11,7 @@ pub fn derive_key(path: String, seed: &Vec<u8>) -> (Vec<u8>, Vec<u8>, Vec<u8>) {
 
     let secret_key: [u8; 32] = extended_secret_key.secret_key.to_bytes();
     let public_key: [u8; 32] = extended_secret_key.public_key().to_bytes();
-    let address: [u8; 32] = derive_address_from_public_key(&public_key);
+    let address: Vec<u8> = derive_address_from_public_key(&public_key);
 
     (secret_key.into(), public_key.into(), address.into())
 }
@@ -21,15 +21,33 @@ fn parse_derivation_path(path: &str) -> DerivationPath {
         .expect("Failed parsing derivation path")
 }
 
-fn derive_address_from_public_key(public_key: &[u8; 32]) -> [u8; 32] {
+fn derive_address_from_public_key(public_key: &[u8; 32]) -> Vec<u8> {
     let mut hasher = Sha3_256::new();
     hasher.update(public_key);
-    hasher.finalize().into()
+    let hash: [u8; 32] = hasher.finalize().into();
+    let mut hash_vector: Vec<u8> = hash.to_vec();
+    hash_vector.insert(0, 0u8);
+    hash_vector.truncate(20);
+    hash_vector
 }
 
-fn test_derive_address_from_public_key(public_key: &[u8; 32]) -> [u8; 32] {
-    let mut hasher = Sha3_512::new();
-    // hasher.update(public_key);
-    // hasher.finalize().into()
-    todo!()
+#[cfg(test)]
+#[allow(non_snake_case)]
+mod tests {
+    use crate::crypto::crypto;
+
+    const PATH: &str = "m/44'/73404'/0'";
+    const SEED: [u8; 64] = [
+        25, 241, 209, 7, 212, 159, 66, 235, 193, 77, 70, 181, 16, 1, 199, 49, 86, 159, 20, 37, 144,
+        253, 210, 1, 103, 221, 238, 219, 178, 1, 81, 103, 49, 173, 90, 201, 181, 141, 58, 28, 156,
+        9, 222, 191, 230, 37, 56, 55, 148, 97, 228, 234, 159, 3, 129, 36, 196, 40, 120, 79, 236,
+        198, 69, 183,
+    ];
+    const ENTROPY: [u8; 32] = [
+        188, 130, 125, 10, 0, 167, 35, 84, 220, 228, 196, 74, 89, 72, 82, 136, 80, 11, 73, 56, 47,
+        155, 168, 138, 1, 99, 81, 120, 123, 123, 21, 202,
+    ];
+
+    #[test]
+    fn test_derive_key() {}
 }
